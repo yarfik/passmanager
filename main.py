fa6b408b92b8bcdfebcc2fa86a49c961a4d6fce2
yarfik <yarfik@gmail.com>
2024-05-29 13:03:52 +0700

    create canvas for logo

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import tkinter as tk
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = tk.Tk()
+window.config(padx=20, pady=20, bg="white")
+window.title("Password Manager")
+
+lock_img = tk.PhotoImage(file="logo.png")
+canvas = tk.Canvas(width=200, height=200, bg="white", highlightthickness=0)
+canvas.create_image(100, 100, image=lock_img)
+canvas.pack()
+
+window.mainloop()
