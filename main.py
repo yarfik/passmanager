44646cdb8f02b576eda4f68f02c09fd97aeb70d7
yarfik <yarfik@gmail.com>
2024-05-31 21:14:03 +0700

    adding random password

--- main.py
+++ main.py
@@ -1,14 +1,69 @@
 import tkinter as tk
+from tkinter import messagebox
+import random
 
+DEFAULT_EMAIL = "yarfik@gmail.com"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = []
+
+[new_item for item in letters]
+for char in range(nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(nr_symbols):
+    password_list += random.choice(symbols)
+
+for char in range(nr_numbers):
+    password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    website = input_website.get()
+    email = input_email.get()
+    pwd = input_password.get()
+
+    is_valid = len(website) > 0 and len(email) > 0 and len(pwd) > 0
+    if not is_valid:
+        messagebox.showerror(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                              f"\nPassword: {pwd} \nIs it ok to save?")
+
+        if is_ok:
+            with open("data.txt", "a") as file_data:
+                file_data.write(f"{website} | {email} | {pwd}\n")
+                input_website.delete(0, 'end')
+                input_email.delete(0, 'end')
+                input_email.insert(0, string=DEFAULT_EMAIL)
+                input_password.delete(0, 'end')
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
+
 window = tk.Tk()
-window.config(padx=20, pady=20, bg="white")
+window.config(padx=50, pady=50, bg="white")
 window.title("Password Manager")
 
 lock_img = tk.PhotoImage(file="logo.png")
@@ -18,22 +73,25 @@ canvas.grid(row=0, column=1)
 
 lbl_website = tk.Label(text="Website: ", bg="white")
 lbl_website.grid(row=1, column=0)
-input_website = tk.Entry(width=35)
-input_website.grid(row=1, column=1, columnspan=2)
-
 lbl_email = tk.Label(text="Email/Username: ", bg="white")
 lbl_email.grid(row=2, column=0)
-input_email = tk.Entry(width=35)
-input_email.grid(row=2, column=1, columnspan=2)
-
 lbl_password = tk.Label(text="Password: ", bg="white")
 lbl_password.grid(row=3, column=0)
-input_password = tk.Entry(width=21)
-input_password.grid(row=3, column=1)
-btn_generate = tk.Button(text="Generate Password")
-btn_generate.grid(row=3, column=2)
 
-btn_add = tk.Button(text="Add", width=36)
-btn_add.grid(row=4, column=1)
+input_website = tk.Entry(width=51, relief="solid")
+input_website.grid(row=1, column=1, columnspan=2, sticky="W")
+input_website.focus()
+
+input_email = tk.Entry(width=51, relief="solid")
+input_email.grid(row=2, column=1, columnspan=2, sticky="W")
+input_email.insert(tk.END, string="yarfik@gmail.com")
+
+input_password = tk.Entry(width=32, relief="solid")
+input_password.grid(row=3, column=1, sticky="W", columnspan=2)
+btn_generate = tk.Button(text="Generate password")
+btn_generate.grid(row=3, column=2, sticky="W")
+
+btn_add = tk.Button(text="Add", width=43, command=save)
+btn_add.grid(row=4, column=1, columnspan=2, sticky="W")
 
 window.mainloop()
